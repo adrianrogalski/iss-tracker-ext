99a95b31c42e226ad73741ea580c9106c0e447fc
adrianrogalski <adrianrogalski99@gmail.com>
2021-12-23 00:19:27 +0100

    change: code refactor

--- src/main/java/service/LogInterfaceApi.java
+++ src/main/java/service/LogInterfaceApi.java
@@ -7,41 +7,48 @@ import model.EventDto;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
+
 import java.util.*;
 
 public class LogInterfaceApi implements LogInterface {
     private final SessionFactory factory;
     private final Map<String, EventDto> events = new HashMap<>();
-
+    private Session session;
     public LogInterfaceApi(SessionFactory factory) {
         this.factory = factory;
     }
 
     @Override
     public boolean saveIntoDb(Scanner scanner, ObjectMapper mapper) throws JsonProcessingException {
-        Session session = factory.openSession();
-        EventDto event;
+        session = factory.openSession();
+        EventDto eventDto;
         while(scanner.hasNextLine()) {
             String jsonStringEvent = scanner.nextLine();
-            event = EventDto.of(jsonStringEvent, mapper);
-            checkAndPrepareEntityObject(event, session);
+            eventDto = EventDto.of(jsonStringEvent, mapper);
+            createEventEntityFrom(eventDto);
         }
         session.close();
         return true;
     }
 
-    private Event checkAndPrepareEntityObject(EventDto eventDto, Session session) {
-        Event entity = new Event();
-        entity.setEventId(eventDto.getId());
-        if (!eventDto.getExtras().isEmpty()) {
-            entity.setExtras(eventDto.getExtras());
-        }
-        flag(eventDto, entity, session);
-        return entity;
+    @Override
+    public void readFromDatabase() {
+        session = factory.openSession();
+        session.createQuery("from Event", Event.class).getResultList().forEach(System.out::println);
+        session.close();
     }
 
-    private void flag(EventDto eventDto, Event entity, Session session) {
+    private Event createEventEntityFrom(EventDto eventDto) {
+        Event event = new Event();
+        event.setEventId(eventDto.getId());
+        if (!eventDto.getExtras().isEmpty()) {
+            event.setExtras(eventDto.getExtras());
+        }
+        calculateDuration(eventDto, event);
+        return event;
+    }
+
+    private boolean calculateDuration(EventDto eventDto, Event entity) {
         if (events.containsKey(eventDto.getId())) {
             final EventDto event = events.get(eventDto.getId());
             final Long time1 = Long.valueOf(eventDto.getTimestamp());
@@ -55,27 +62,19 @@ public class LogInterfaceApi implements LogInterface {
             }
             entity.setDuration(duration);
             events.remove(eventDto.getId());
-            save(entity, session);
+            save(entity);
+            return true;
         }
         else {
             events.put(eventDto.getId(), eventDto);
+            return false;
         }
     }
 
-    private void save(Event event, Session session) {
+    private void save(Event event) {
         Transaction transaction = session.beginTransaction();
         session.save(event);
         transaction.commit();
     }
-
-    public void readFromDatabase() {
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-        final Query<Event> from_event = session.createQuery("from Event", Event.class);
-        final List<Event> resultList = from_event.getResultList();
-        System.out.println("asadasd");
-        resultList.forEach(System.out::println);
-        transaction.commit();
-        session.close();
-    }
 }
+
